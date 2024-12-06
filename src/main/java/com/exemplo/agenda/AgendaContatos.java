d7f8e5648c3921457bae3486fcc3ca0640e0549b
Nicolas Beloto <nicolasbelotoarmenio9@gmail.com>
2024-12-06 19:25:39 -0300

    Atividade Finalizada

--- src/main/java/com/exemplo/agenda/AgendaContatos.java
+++ src/main/java/com/exemplo/agenda/AgendaContatos.java
@@ -10,7 +10,7 @@ import com.exemplo.agenda.controller.ContatoController;
 /**
 * Classe principal que inicializa a aplicação Agenda de Contatos.
 * Responsável por criar as instâncias dos componentes MVC e iniciar a interface.
-* @author Nicola Beloto
+* @author Nicolas Beloto
 * @version 1.0
  */
 public class AgendaContatos {
